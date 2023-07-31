1dbd655bc46c99b9268c259588379cbe5beb32bd
Urboiihunter <hunterstetson35@gmail.com>
2023-07-31 18:05:35 -0400

    Please enter the commit message for your changes. Lines starting

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -8,28 +8,147 @@ int extraMemoryAllocated;
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
-	
+    {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    
+    // Create temporary arrays for the left and right subarrays
+    int* L = (int*)malloc(sizeof(int) * n1);
+    int* R = (int*)malloc(sizeof(int) * n2);
+    
+    // Copy data to the temporary arrays
+    for (i = 0; i < n1; i++)
+    {
+        L[i] = pData[l + i];
+    }
+    for (j = 0; j < n2; j++)
+    {
+        R[j] = pData[m + 1 + j];
+    }
+    
+    i = 0;
+    j = 0;
+    k = l;
+    
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            pData[k] = L[i];
+            i++;
+        }
+        else
+        {
+            pData[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    // Copy the remaining elements of L[] and R[]
+    while (i < n1)
+    {
+        pData[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        pData[k] = R[j];
+        j++;
+        k++;
+    }
+    
+    // Free the memory allocated for temporary arrays
+    free(L);
+    free(R);
+}
+
+void mergeSort(int pData[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        
+        // Sort the first and second halves
+        mergeSort(pData, l, m);
+        mergeSort(pData, m + 1, r);
+        
+        // Merge the sorted halves
+        merge(pData, l, m, r);
+    }
+}
 }
 
 // implement insertion sort
 // extraMemoryAllocated counts bytes of memory allocated
 void insertionSort(int* pData, int n)
 {
-	
+    for (int i = 1; i < n; i++)
+    {
+        int key = pData[i];
+        int j = i - 1;
+        
+        // Move elements of pData[0..i-1] that are greater than key to one position ahead of their current position
+        while (j >= 0 && pData[j] > key)
+        {
+            pData[j + 1] = pData[j];
+            j--;
+        }
+        
+        pData[j + 1] = key;
+        
+        // Increment extraMemoryAllocated to account for the temporary variables (key and j)
+        extraMemoryAllocated += 2 * sizeof(int);
+    }
 }
 
 // implement bubble sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void bubbleSort(int* pData, int n)
 {
-	
+     for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (pData[j] > pData[j + 1])
+            {
+                // Swap the elements at j and j+1
+                int temp = pData[j];
+                pData[j] = pData[j + 1];
+                pData[j + 1] = temp;
+                
+                // Increment extraMemoryAllocated to account for the temporary variable (temp)
+                extraMemoryAllocated += sizeof(int);
+            }
+        }
+    }
 }
 
 // implement selection sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void selectionSort(int* pData, int n)
 {
-	
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (pData[j] < pData[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        
+        // Swap the elements at i and minIndex
+        int temp = pData[i];
+        pData[i] = pData[minIndex];
+        pData[minIndex] = temp;
+        
+        // Increment extraMemoryAllocated to account for the temporary variable (temp)
+        extraMemoryAllocated += sizeof(int);
+    }
 }
 
 // parses input file to an integer array
